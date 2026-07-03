860746fb2330f433b061f041dd1ef7ae77de4b25
Srijon Das <dassrijon52@gmail.com>
2026-07-03 11:37:46 +0530

    Changes in Server.js

--- routers/registerroutes/registerroutes.js
+++ routers/registerroutes/registerroutes.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const registerController = require("../../controllers/registercontroller/registercontroller");
+const registerController = require("../../controllers/registercontroller/registerController");
 
 // Routes
 router.post("/create-register", registerController.createregister);
